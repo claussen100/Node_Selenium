66664f1315076241d9b3eeeaac569ca2aa2609ed
claussen100 <claussenpatrick@gmail.com>
2024-10-04 01:09:37 -0300

    Salvando dados do scrapping, no banco de dados

--- scrapping_Oscar_films.js
+++ scrapping_Oscar_films.js
@@ -1,4 +1,8 @@
 import { Builder, Browser, By, Key } from "selenium-webdriver";
+import pkg from "pg";
+import dotenv from "dotenv";
+const { Client } = pkg;
+dotenv.config();
 
 (async function scrapping_oscar_films() {
   let driver = await new Builder().forBrowser(Browser.CHROME).build();
@@ -9,17 +13,26 @@ import { Builder, Browser, By, Key } from "selenium-webdriver";
     .findElement(By.xpath("//*[@id='APjFqb']"))
     .sendKeys("List of Academy Award winning films", Key.ENTER);
 
-  await driver.findElement(By.xpath("//*[@id='rso']/div[1]")).click(); //Estudar uma melhor captura de campo ao invés do xpath neste cenário.
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+
+  await driver.findElement(By.xpath("//*[@id='rso']/div[1]")).click();
 
   let table = await driver.findElement(
     By.className("wikitable sortable jquery-tablesorter")
   );
 
   let rows = await table.findElements(By.css("tr"));
-
   let tableData = [];
+  let maxRows = 206; //Limite para captura de filmes entre 2010 - 2024
+  let rowCount = 0;
 
   for (let row of rows) {
+    if (rowCount >= maxRows) break;
+
     let cells = await row.findElements(By.css("td"));
     let rowData = [];
 
@@ -30,12 +43,42 @@ import { Builder, Browser, By, Key } from "selenium-webdriver";
 
     if (rowData.length > 0) {
       tableData.push(rowData);
+      rowCount++;
     }
   }
 
-  let filteredData = tableData.filter((row) => {
-    return row[1] && Number(row[1]) > 2000;
+  const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT, 10),
   });
 
-  console.log(JSON.stringify(filteredData, null, 2));
+  await client.connect();
+
+  const jsonData = JSON.stringify(tableData, null, 2);
+  console.log(jsonData);
+
+  const insertData = async () => {
+    try {
+      for (let item of tableData) {
+        const query = `INSERT INTO award_winning_films (film, year, awards, nominations) VALUES ($1, $2, $3, $4)`;
+        const values = [
+          item[0],
+          parseInt(item[1]),
+          parseInt(item[2]),
+          parseInt(item[3]),
+        ];
+        await client.query(query, values);
+      }
+      console.log("Dados inseridos com sucesso");
+    } catch (err) {
+      console.error("Erro ao inserir os dados", err);
+    } finally {
+      await client.end();
+    }
+  };
+
+  await insertData();
 })();
